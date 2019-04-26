92d695ba338371b6e6db6b74a2765f5a0e96ac3c
ddbzdnql <gdbzdnql@gmail.com>
2019-04-26 06:04:44 -0700

    from mac

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = function(){
+		return {
+			words: {
+			   	{
+					   	Name: "abase",
+						Explanation: "lower; degrade; humiliate; make humble; make (oneself) lose self-respect",
+						Index: 1
+			   	},
+				{
+							   Name: "word 2",
+							   Explanation: "placeholder for real words with explanations to be populated later",
+							   Index: 2
+					   },
+					   {
+							   Name: "word 3",
+							   Explanation: "placeholder for real words with explanations to be populated later",
+							   Index: 3
+					   },
+					   {
+							   Name: "word 4",
+							   Explanation: "placeholder for real words with explanations to be populated later",
+							   Index: 4
+					   },
+					   {
+							   Name: "word 5",
+							   Explanation: "placeholder for real words with explanations to be populated later",
+							   Index: 5
+					   }
+	   			}
+		}
+}
